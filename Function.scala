ce1a7e9e0bf085e48ad65d565540fb8dd93e9e29
gympohnpimol <gympohnpimol@gmail.com>
2020-12-03 20:53:46 +0900

    function

---
+++ Function.scala
@@ -0,0 +1,14 @@
+
+object TryFunctions extends App {
+
+    def mySum(a: Int, b: Int): Int = {
+        println("In mySum Function")
+        a + b
+    }
+
+    def hello(name: String, id: Int): Unit = {
+        println("Hello " + name + "  ID: " + id)
+    }
+    println(mySum(10,8))
+    hello(name = "Gym", id = 2110)
+}
